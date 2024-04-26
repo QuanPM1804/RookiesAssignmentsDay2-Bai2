a58a82b4d67e998e73f581d8aa46286c06f13b64
QuanPM <95131155+QuanPM1804@users.noreply.github.com>
2024-04-26 15:46:05 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PrimeNumberAsyncApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int startRange = 0;
+            int endRange = 100;
+            List<int> primes = await GetPrimeNumbersAsync(startRange, endRange);
+            Console.WriteLine ("List prime number: ");
+            foreach (var num in primes)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        static async Task<List<int>> GetPrimeNumbersAsync(int start, int end)
+        {
+            List<int> primes = new List<int>();
+
+            await Task.Run(() =>
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    if (IsPrime(i))
+                    {
+                        primes.Add(i);
+                    }
+                }
+            });
+            return primes;
+        }
+        static bool IsPrime(int num)
+        {
+            if (num <= 1)
+            {
+                return false;
+            }
+            if (num == 2)
+            {
+                return true;
+            }
+            if (num % 2 == 0)
+            {
+                return false;
+            }
+            int sqrt = (int)Math.Sqrt(num);
+            for (int i = 3; i <= sqrt; i += 2)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
